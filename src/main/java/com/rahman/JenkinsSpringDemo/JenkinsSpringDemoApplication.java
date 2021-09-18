e3be49d0a9fa6d20e6341153b4c9f0bf65e3bc06
Jawadur Rahman <jawadur@mss.com>
2021-09-18 19:55:14 +0300

    Second Commit

--- src/main/java/com/rahman/JenkinsSpringDemo/JenkinsSpringDemoApplication.java
+++ src/main/java/com/rahman/JenkinsSpringDemo/JenkinsSpringDemoApplication.java
@@ -1,12 +1,25 @@
 package com.rahman.JenkinsSpringDemo;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsSpringDemoApplication {
+	
+	public static Logger logger=(Logger) LoggerFactory.getLogger(JenkinsSpringDemoApplication.class);
+	
+	@PostConstruct
+	public void intt() {
+		
+		logger.info("Application Started.....");
+	}
 
 	public static void main(String[] args) {
+		logger.info("Application Started.1111....");
 		SpringApplication.run(JenkinsSpringDemoApplication.class, args);
 	}
 
