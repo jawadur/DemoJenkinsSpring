d0ec3518ddd325c16bc91cb4fb68dc939efe8af2
Jawadur Rahman <jawadur@mss.com>
2021-09-19 00:21:32 +0300

    Fourth Commit

--- src/test/java/com/rahman/JenkinsSpringDemo/JenkinsSpringDemoApplicationTests.java
+++ src/test/java/com/rahman/JenkinsSpringDemo/JenkinsSpringDemoApplicationTests.java
@@ -4,16 +4,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class JenkinsSpringDemoApplicationTests {
+public class JenkinsSpringDemoApplicationTests {
 
 	public static Logger logger=(Logger) LoggerFactory.getLogger(JenkinsSpringDemoApplicationTests.class);
 	
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 		logger.info("Test Application Started.....");
 		assertEquals(true,true);
 	}
